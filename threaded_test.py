1b3901cb4d3c97e5708b5fe7757cbc57823c882e
Janne Enberg <janne.enberg@lietu.net>
2016-04-25 23:47:23 +0300

    Initial threaded ws4py based client, seems to work

---
+++ threaded_test.py
@@ -0,0 +1,22 @@
+from pypusu.threaded import PuSuClient
+
+if __name__ == "__main__":
+    c = PuSuClient("ws://127.0.0.1:55000")
+
+    count = 0
+
+
+    def listener(msg):
+        global count
+        count += 1
+
+
+    c.authorize("foo")
+    c.subscribe("channel.1", listener)
+    c.publish("channel.1", {"foo": "bar"})
+
+    from time import sleep
+
+    sleep(30)
+
+    print(count)
