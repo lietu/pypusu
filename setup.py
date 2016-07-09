d59e15f2987ac9a6a6281783e6b5960ffe0bfbae
Janne Enberg <janne.enberg@lietu.net>
2016-07-09 19:25:26 +0300

    Making it v1.0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pypusu',
     packages=['pypusu'],  # this must be the same as the name above
-    version='1.0.1',
+    version='1.0.2',
     description='Python client for PuSuEngine',
     author='Janne Enberg',
     author_email='janne.enberg@lietu.net',
     url='https://github.com/PuSuEngine/pypusu',
-    download_url='https://github.com/PuSuEngine/pypusu/tarball/v1.0.1',
+    download_url='https://github.com/PuSuEngine/pypusu/tarball/v1.0.2',
     keywords=['pubsub', 'publisher', 'subscriber', 'messaging'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
